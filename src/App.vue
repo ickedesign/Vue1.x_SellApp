08eeac147f4c73f1a2780c32c1a66e1a85e76fe9
icke <619436245@qq.com>
2017-07-04 20:13:14 +0800

    use VueResource

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
     <div class="tab border-1px">
       <div class="tab-item">
         <a v-link="{path:'/goods'}">商品</a>
@@ -18,7 +18,26 @@
 
 <script type="text/ecmascript-6">
   import header from 'components/header/header.vue';
+
+  const ERR_OK = 0;
+
   export default {
+    data() {
+      return {
+        seller: {}
+      };
+    },
+    // Vue的生命钩子,请求数据并返回Promise，一个异步过程
+    created() {
+      // this指的是Vue对象
+      this.$http.get('/api/seller').then((response) => {
+        // response.body是个对象
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.seller = response.data;
+        }
+      });
+    },
     components: {
       'v-header': header
     }
@@ -26,8 +45,8 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  @import "common/stylus/mixin"
-  
+  @import "./common/stylus/mixin"
+
   .tab
     display: flex
     width: 100%
